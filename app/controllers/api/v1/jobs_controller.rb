5dd23a5f24f3b6e68c397c4dc1f2e8411949b924
Mayank Kumar <mayankkumarj>
2021-11-17 15:27:36 +0530

    Added create job api

--- app/controllers/api/v1/jobs_controller.rb
+++ app/controllers/api/v1/jobs_controller.rb
@@ -3,6 +3,17 @@
 module Api
   module V1
     class JobsController < ApplicationController
+
+      def create
+        job = JobCreation.new(job_params.merge({ created_by: 'api' })).create_job
+
+        if job.errors.present?
+          render json: job.errors, status: :unprocessable_entity
+        else
+          render json: job, status: :created
+        end
+      end
+
       def process_job
         job_ids = Array(params[:job_id])
 
@@ -12,6 +23,12 @@ module Api
           jobs.each(&:progress!)
         end
         render json: jobs, status: :created
+      end
+      
+      private
+
+      def job_params
+        params.require(:job).permit(:name, :priority, :triggered_at)
       end
     end
   end
--- lib/job_creation.rb
+++ lib/job_creation.rb
@@ -3,13 +3,14 @@
 class JobCreation
   def initialize(options = {})
     options = options.instance_of?(Array) ? Hash[*options] : options
+    @created_by    = options.delete('created_by')
     @params        = options
   end
 
   def create_job
     job = Job.new(@params)
     if job.save && job.triggered_at.nil?
-      job.invoke_job
+      job.invoke_job unless @created_by == 'api'
     end
     job
   end
